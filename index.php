16b9e7d994207eedae2d8646f449d4a90a86f9bf
Skarvion <williams.saputra@gmail.com>
2018-10-31 23:03:50 +1100

    init

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+$id = 100039787;
+$name = "William";
+$gpa = 3.4;
+$units = array(
+    "Software Engineering Project B",
+    "Software Deployment and Evolution",
+    "Language in Software Development",
+    "Research Reports A"
+);
+
+echo "List of values <br /><br />";
+
+echo "ID: ";
+var_dump($id);
+echo "<br />";
+
+echo "Name: ";
+var_dump($name);
+echo "<br />";
+
+echo "GPA: ";
+var_dump($gpa);
+echo "<br />";
+
+echo "Units: ";
+var_dump($units);
+echo "<br />";
+
+?>
